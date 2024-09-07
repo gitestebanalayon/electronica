966b324769579dc8cfbaa27b03f138bf0ff9369a
gitesteban0 <gitestebanalayon@gmail.com>
2024-09-07 13:27:42 -0400

    ok

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,25 +1,41 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+
+      isGlobal: true, // Hace que ConfigModule esté disponible en todo el módulo
+      envFilePath: '.env', // Especifica el archivo .env
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD:
+          Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('database.host'),
-        port: configService.get<number>('database.port'),
-        username: configService.get<string>('database.username'),
-        password: configService.get<string>('database.password'),
-        database: configService.get<string>('database.name'),
-        autoLoadEntities: true,
-        synchronize: false,
-        logging: true,
-      }),
+      useFactory:
+        async (configService: ConfigService) => ({
+          type: 'postgres',
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.username'),
+
+          password: configService.get<string>('database.password'),
+          database: configService.get<string>('database.name'),
+          autoLoadEntities: true,
+
+          synchronize: false, // ¡No se recomienda en producción!
+          logging: true, // Útil para depuración
+        }),
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
