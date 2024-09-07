5c2c1608c736b5f7b1e9bd3907d65c1455b3c8d3
gitesteban0 <gitestebanalayon@gmail.com>
2024-09-07 14:11:32 -0400

    restar db

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,22 +5,22 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
         type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        url: process.env.DATABASE_URL,
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
         autoLoadEntities: true,
         synchronize: false,
         logging: true,
-      }),
     }),
   ],
 })
 export class DatabaseModule {}
+
