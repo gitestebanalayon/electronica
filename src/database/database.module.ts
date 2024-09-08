6a231096e6de8f74c55a136b2a9dd0b32cb0b30a
gitesteban0 <gitestebanalayon@gmail.com>
2024-09-08 00:35:21 -0400

    sadas

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,23 +5,25 @@ import { ConfigModule } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      //url: process.env.DATABASE_URL, //Activar para modo remoto
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
+        type: 'postgres',
+        url: process.env.DATABASE_URL, //Activar para modo remoto
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
     }),
   ],
 })
 export class DatabaseModule {}
+
